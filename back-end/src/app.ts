839e233ba377ec34849b1bfe187a46d8d555b198
daniel delos silva <apeiron52@gmail.com>
2023-04-05 14:31:50 -0300

    fix: cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,11 +1,14 @@
 import "express-async-errors";
 import express, { Application } from "express";
+import cors from "cors"
 import { handleErrors } from "./errors";
 import userRoutes from "./routes/users.routes";
 import loginRoutes from "./routes/login.routes";
 import contactsRoutes from "./routes/contacts.routes";
 
+
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
@@ -14,3 +17,5 @@ app.use("/contacts", contactsRoutes);
 
 app.use(handleErrors);
 export default app;
+
+
